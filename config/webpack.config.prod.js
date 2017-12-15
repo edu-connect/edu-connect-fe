aab74ebd66cc4555c950fee9132b408b41113f5e
Jeewhan <distinct19@gmail.com>
2017-12-15 19:31:45 +0900

    CSS Module 적용

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -179,6 +179,9 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        modules: true,
+                        localIdentName:
+                          "[path][name]__[local]--[hash:base64:5]",
                       },
                     },
                     {
