e8bca2467e49170bc806ab2264a8b6b25939c2a5
Jeewhan <distinct19@gmail.com>
2017-12-18 01:39:12 +0900

    react-datetime 작동을 확인하기 위한 Landing Component 생성
    * 대부분의 프로젝트 구조 관련 아티클들이 Redux에 대한 고민의 결과여서, Redux를 사용하지 않을 이 프로젝트는 나름의 방향을 찾아야 할 것으로 예상됩니다.
    * 그래서 프로젝트 구조에 대한 큰 고민은 문제를 마주칠 때까지 미뤄두었습니다.
    * 8번 라인의 ../../는 수정하고 싶었으나 babel-loder에 includePaths와 같은 옵션이 보이지 않아 우선은 그대로 두었습니다.

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import moment from "moment";
+import { Button } from "semantic-ui-react";
+
+// https://github.com/moment/moment/issues/2962#issuecomment-255637859
+import "moment/locale/ko";
+
+import InputTime from "../../common/InputTime/InputTime";
+
+const timePickerData = [
+  { cn: "start-time", hour: 9, type: "startTime" },
+  { cn: "end-time", hour: 22, type: "endTime" }
+];
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      startTime: { hour: 9, minute: 0 },
+      endTime: { hour: 22, minute: 0 }
+    };
+
+    this.handleInputTime = this.handleInputTime.bind(this);
+  }
+
+  handleInputTime(time, type) {
+    const hour = time.hour();
+    const minute = time.minute();
+
+    this.setState({ [type]: { hour, minute } });
+  }
+
+  renderInputTime() {
+    return timePickerData.map(({ cn, type, hour }) => (
+      <InputTime
+        cn={cn}
+        type={type}
+        initValue={moment({ hour })}
+        handleInputTime={this.handleInputTime}
+      />
+    ));
+  }
+
+  render() {
+    const { startTime, endTime } = this.state;
+
+    return (
+      <div>
+        <p>{`startTime: ${moment(startTime).format("LT")}`}</p>
+        <p>{`endTime: ${moment(endTime).format("LT")}`}</p>
+        {this.renderInputTime()}
+        <Button>버튼</Button>
+      </div>
+    );
+  }
+}
+
+export default Landing;
