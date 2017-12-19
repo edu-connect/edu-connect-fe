6ce60c32150e831469f63dea3fc23a6c5fa7de68
edusqaud <edusquad@gmail.com>
2017-12-19 09:24:14 +0900

    trvavis test!!!

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo $TRAVIS_BRANCH
 elif [ "$TRAVIS_BRANCH" = "develop" ]; then
-  curl -X GET http://ec2-13-125-21-21.ap-northeast-2.compute.amazonaws.com:9999/deploy/FE
+  curl -X GET http://ec2-13-125-21-21.ap-northeast-2.compute.amazonaws.com:9999/deploy/test
 elif [ "$TRAVIS_BRANCH" = "feature/*" ]; then
   echo $TRAVIS_BRANCH
 fi
